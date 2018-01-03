de34b3bc633b44db39f8568542fc2331835f1e5d
Ahmed-p <35038073+Ahmed-p@users.noreply.github.com>
2018-01-03 16:40:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by Ahmed');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
+});
+
 // playing
 client.on('ready', () => {                           
-	client.user.setGame(``,'https://www.twitch.tv/Ahmed');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`(:|On 1 Servers`,'https://www.twitch.tv/Ahmed');
-	});          
-});
+client.user.setGame(``,'https://www.twitch.tv/Ahmed');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`.help | On 1 Servers`,'https://www.twitch.tv/Ahmed');
+});          
 
 
 client.on('message', message => {
